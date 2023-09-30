ca4d9d43310dc3bd1e5c39cc515ddc0b482c449b
Artin <artinnikjoo88@gmail.com>
2023-09-30 20:05:48 +0330

    Added files

---
+++ QRgenerator.py
@@ -0,0 +1,100 @@
+from qrcode import QRCode
+import tkinter as tk
+from tkinter import font
+import os
+
+if __name__ == "__main__":
+    print(">> welcome to QRcode generator v1.0, developed by: Artin (Ketchup d) <<")
+
+path = "Qrcode"
+isExist = os.path.exists(path)
+if not isExist:
+    os.makedirs(path)
+
+
+def ofl(a=1):
+    os.startfile("Qrcode")
+
+
+def qrcode():
+    data = entry.get()
+    qr = QRCode(version=1, box_size=10, border=3)
+    qr.add_data(data)
+    img = qr.make_image(fill_color="black", back_color="white")
+    if len(entryname.get()) >= 1:
+        img.save(f"Qrcode\\{entryname.get()}.png")
+    else:
+        img.save("Qrcode\\qrcode.png")
+    labeldone.pack(pady=15)
+
+
+window = tk.Tk()
+window.title("QRcode generator")
+window.geometry("510x470")
+window.config(bg="#1A1A1A")
+
+labelcopyright = tk.Label(text="© 2023 - QRgenerator",
+                          bg="#1A1A1A", fg="#949494").pack(side="bottom")
+
+photo = tk.PhotoImage(
+    file="models\\qrcodeicon.png")
+window.iconphoto(False, photo)
+
+menubar = tk.Menu(window, background="black", fg="white")
+
+file = tk.Menu(menubar, tearoff=False)
+
+file.add_command(label="Open files locations",
+                 command=ofl, accelerator="Ctrl+F")
+file.add_command(label="Close", command=window.quit, accelerator="Alt+F4")
+
+menubar.add_cascade(label="File", menu=file)
+
+frame_url = tk.Frame(window, bg="#1A1A1A")
+frame_url.pack(pady=40)
+
+frame_name = tk.Frame(window, bg="#1A1A1A")
+frame_name.pack()
+
+frame_button = tk.Label(window, bg="#1A1A1A")
+frame_button.pack(pady=30)
+
+frame_message = tk.Label(window, bg="#1A1A1A")
+frame_message.pack(side="bottom")
+
+font_medium = font.Font(size=14)
+font_small = font.Font(size=12)
+font_xsmall = font.Font(size=10)
+
+label = tk.Label(frame_url, text="Enter the url/data you want to get as an QRcode:",
+                 font=font_medium, bg="#1A1A1A", fg="white")
+label.pack()
+
+entry = tk.Entry(frame_url, width=45, font=font_small,
+                 bg="#212121", fg="white")
+entry.pack(pady=15)
+
+labelname = tk.Label(frame_name, text="Save as (file name):",
+                     font=font_medium, bg="#1A1A1A", fg="white")
+labelname.pack()
+label_optinal = tk.Label(frame_name, bg="#1A1A1A",
+                         fg="#f0f0f0", text="(Optional)", font=font_xsmall)
+label_optinal.pack()
+
+entryname = tk.Entry(frame_name, width=15, font=font_small,
+                     bg="#212121", fg="white")
+entryname.pack(pady=15)
+
+labeldone = tk.Label(frame_message, text="done, your qrcode has been saved\n as an image in the qrcode folder in current directory.",
+                     font=font_xsmall, bg="#1A1A1A", fg="#C0C0C0")
+
+button = tk.Button(frame_button, text="Done", command=qrcode,
+                   font=font_small, bg="#1A1A1A", fg="white", width=10, height=1)
+button.pack()
+
+window.config(menu=menubar)
+
+window.bind_all("<Control-f>", ofl)
+
+if __name__ == "__main__":
+    window.mainloop()
---
+++ README.md
@@ -0,0 +1,12 @@
+# QRcode generator
+You can generate your custom QRcode with this cool Tkinter application!
+
+## Feutures
+- Generated QRcode will be saved as an .png image in a folder in current directory.
+- Dark theme
+
+## Authors
+- [@Ketchup-d](https://github.com/Ketchup-d)
+
+## License
+[MIT License](LICENSE)
